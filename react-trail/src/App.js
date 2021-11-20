9b7439b10f425e761e72c8e8e2c96698e0151aac
Arzan101 <khanazzu994@gmail.com>
2021-11-20 22:18:22 +0530

    commit on main

--- react-trail/src/App.js
+++ react-trail/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+ const state = useState();
+ console.log(state);
+
+function cart(){
+  //count++
+}
+return(
+    <div>
+      <h1>0</h1>
+    <button onClick={cart}>Add</button>
     </div>
+    // document.getElementById("root")
   );
 }
 
-export default App;
+export default App
+
