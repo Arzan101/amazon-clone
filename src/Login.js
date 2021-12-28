d1095523c83c35173eb659f2cb77e3e71811b2a4
Arzan101 <khanazzu994@gmail.com>
2021-12-28 20:11:52 +0530

    commit on master

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
     return (
         <div className='login'>
+        <Link to = "/">
+        <img className = "header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Logo"/>  
+        </Link> 
             
         </div>
     )
