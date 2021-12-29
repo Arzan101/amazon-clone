c6851bf6997a05fc315a5b5b613cb1c57c218505
Arzan101 <khanazzu994@gmail.com>
2021-12-29 21:46:54 +0530

    commit on master

--- src/Login.js
+++ src/Login.js
@@ -7,8 +7,7 @@ function Login() {
         <div className='login'>
         <Link to = "/">
         <img className = "login__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png" alt="Logo"/>  
-        </Link> 
-            
+        </Link>    
         </div>
     )
 }
