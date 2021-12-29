53619f73be3f0bc60e330de4a057ebaf4aac286e
Arzan101 <khanazzu994@gmail.com>
2021-12-29 22:04:05 +0530

    commit on master

--- src/Login.js
+++ src/Login.js
@@ -7,7 +7,21 @@ function Login() {
         <div className='login'>
         <Link to = "/">
         <img className = "login__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png" alt="Logo"/>  
-        </Link>    
+        </Link>
+        <div className='login__container'>
+        <h1>Sign-In</h1>
+        <form>
+        <h5>E-mail</h5>
+        <input Type="text"/>
+        <h5>Password</h5>
+        <input Type="password"/>
+        <button className='login--signButton'>Sign In</button>
+        </form>
+        <p>
+        By creating an account or logging in, you agree to Amazon’s Conditions of Use and Privacy Policy.
+        </p>
+        <button className='login--registerButton'>Create your Amazon Account</button>
+        </div>    
         </div>
     )
 }
