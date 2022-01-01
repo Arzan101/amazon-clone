3621e3c5a0ac467a8ac4650da017d805ab2e5d53
Arzan101 <khanazzu994@gmail.com>
2022-01-01 20:56:46 +0530

    commit on master

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDLFLu6RY9ArlTww_NPV_7Ws-N61FYGgnQ",
   authDomain: "clone-a4bf9.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "150366921500",
   appId: "1:150366921500:web:934219f2ad53251d6950d9"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export{db,auth};
