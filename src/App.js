34c6d08c5058a6655baa1272f11170070279563d
Arzan101 <khanazzu994@gmail.com>
2022-01-02 21:36:48 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home'
 import Checkout from './Checkout'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  
+  useEffect(() =>{
+    //will only run once when app component is loaded.
+    auth.onAuthStateChanged(
+      authUser => {
+        console.log('THE USER IS :', authUser);
+     
+      if (authUser){
+        //user just logged in / user was logged in
+      }
+      else{
+        //user logged out
+      }
+    })
+  }, [])
+
+  
   return (
    <Router>
     <div className="app">
