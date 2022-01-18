bd28f42dfe0dd1c09bab49996a194938a8353d0d
Arzan101 <khanazzu994@gmail.com>
2022-01-18 19:56:53 +0530

    commit on master

--- src/Header.js
+++ src/Header.js
@@ -39,7 +39,7 @@ function Header() {
         <span className="header__optionlineOne">
                  Hello
         </span>
-        <span className="header__optionlineTwo" >{user ?'SignOut':'SignIn'}
+        <span className="header__optionlineTwo" >{user ? 'SignOut' : 'SignIn'}
         </span>
         </div>
     </Link>
