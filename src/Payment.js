ae575c7e0f21bed3c483d719a31aa1914bbc7515
Arzan101 <khanazzu994@gmail.com>
2022-03-01 12:04:41 +0530

    commit on master

--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./Payment.css"
+import { useStateValue } from './StateProvider';
 
 function Payment() {
+    const [{cart,user},dispatch] = useStateValue();
   return (
     <div className='payment'>
        <div className='payment--container'>
        {/*delivery address*/}
          <div className='payment--section'>
+          <h3>Delivery Address:</h3>
+          <div className='payment--address'>
+            <p>{user?.email}</p>
+            <p>flat 302, Hiranandani</p>
+            <p>Powai,india</p>
+          </div>
 
          </div>
            {/*cart review*/}
