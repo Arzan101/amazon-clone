a0694f67e72fddc6571aaed8b4250cf8471acdb4
Arzan101 <khanazzu994@gmail.com>
2022-03-01 11:36:38 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -47,6 +48,10 @@ function App() {
               <Route path="/checkout">
                       <Header />
                       <Checkout /> 
+              </Route>
+              <Route path="/payment">
+                      <Header />
+                      <Payment />
               </Route>
               <Route path="/">
                       <Header />
