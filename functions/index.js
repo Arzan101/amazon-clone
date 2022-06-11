98d3519cfaecac9e5dbae516cc4f413ea183060d
Arzan101 <khanazzu994@gmail.com>
2022-06-11 17:30:42 +0530

    commit on master

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 const functions = require("firebase-functions");
-const express = require("express");
-const cors = require("cors");
-const stripe = require("stripe")("");
+const express = require("express"); //npm i express
+const cors = require("cors"); //npm i cors
+const stripe = require("stripe") ("sk_test_51KZRkQSHV4CwkIjNKPpmd6J4r35VaJmniTLGWsvYKgsrKoYKkvwC5C3NpGPA5aF7nO8sZA85WOBBqlcLGfutCRGr00HEVbaxAU"); //npm i stripe 
