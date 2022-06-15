a159b4339b0991ad96277517899aa71e77fc9dda
Arzan101 <khanazzu994@gmail.com>
2022-06-15 18:52:17 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
       <div className="App">
         <Switch>
                <Route path="/orders">
+                <Header />
                 <Orders />
               </Route>
               <Route path="/login">
