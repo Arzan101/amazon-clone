c358ab14386bd4381b2e533f9c70f2aa9d3fa218
Arzan101 <khanazzu994@gmail.com>
2022-06-15 18:40:43 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import orders from "./Orders";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import Payment from "./Payment";
