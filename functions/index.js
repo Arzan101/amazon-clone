dc3e325bbdcef5f80a2b9f5f1ef03d1189104e40
Arzan101 <khanazzu994@gmail.com>
2022-06-17 21:25:45 +0530

    commit on master

--- functions/index.js
+++ functions/index.js
@@ -13,20 +13,20 @@ const app = express();
 app.use(cors({origin: true}));
 app.use(express.json());
 // - API Routes
-app.get('/', (request, response)=>response.status(200).send('hello world'));
+app.get("/", (request, response)=>response.status(200).send('hello world'));
 
-app.post('/payment/create', async(request, response) =>{
+app.post("/payment/create",async(request, response) =>{
    const total = request.query.total;
-   console.log("Total payment received>>", total);
+   console.log("Total Rqeuest Total", total);
    const paymentIntent = await stripe.paymentIntent.create({
     amount : total, // -Paise wali value mai dega rupee mai nhi
     currency:"inr",
    });
 
    response.status(201).send({
-      clientSecret: paymentIntent.client-secret,
+      clientSecret: paymentIntent.client_secret,
    })
-})
+});
 
 // -Listen Command
 
